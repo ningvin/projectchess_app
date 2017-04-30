1e094d76c43edd9b237dd27d326c117135e7d1f6
Julius Bettin <julius.bettin@gmail.com>
2017-04-30 17:00:28 +0900

    Refactor page lifecycle management

--- www/js/index.js
+++ www/js/index.js
@@ -1,34 +1,71 @@
 (function() {
     var app;
+    
+    var pages = {
+        
+        'main-menu': (function() {
+            
+            return {
+                init: function(page) {
+                    page.querySelector('#loginbtn').onclick = function() {
+                        document.querySelector('#navigator')
+                                .pushPage('pages/login.html');
+                    };
 
-    var pageInitializers = {
+                    page.querySelector('#regbtn').onclick = function() {
+                        document.querySelector('#navigator')
+                                .pushPage('pages/register.html');
+                    };
+                },
+                
+                show: function(page) {
+                    
+                },
+                
+                hide: function(page) {
+                    
+                },
+                
+                destroy: function(page) {
+                    
+                }
+            }
+            
+        })(),
         
-        'main-menu': function(page) {
-            page.querySelector('#loginbtn').onclick = function() {
-                document.querySelector('#navigator').pushPage('pages/login.html');
-            };
-
-            page.querySelector('#regbtn').onclick = function() {
-                document.querySelector('#navigator').pushPage('pages/register.html');
-            };
-        },
+        'register-page': (function() {
+            
+            return {
+                init: function(page) {
+                    page.querySelector('ons-toolbar .center').innerHTML = 'Register';
+                    page.querySelector('#cancel-btn').onclick = function() {
+                        document.querySelector('#navigator').popPage();
+                    };
+                },
+                
+                show: function(page) {
+                    
+                },
+                
+                hide: function(page) {
+                    
+                },
+                
+                destroy: function(page) {
+                    
+                }
+            }
+            
+        })(),
         
-        'register-page': function(page) {
-            page.querySelector('ons-toolbar .center').innerHTML = 'Register';
-            page.querySelector('#cancel-btn').onclick = function() {
-                document.querySelector('#navigator').popPage();
-            };
-        },
-        
-        'login-page': function(page) {
-            page.querySelector('ons-toolbar .center').innerHTML = 'Login';
-            page.querySelector('#cancel-btn').onclick = function() {
-                document.querySelector('#navigator').popPage();
-            };
-            page.querySelector('#loginbtn').onclick = function() {
+        'login-page': (function() {
+            
+            var _page;
+            
+            var _handleLogin = function() {
                 var data = {
-                    alias: page.querySelector('#username').value,
-                    password: page.querySelector('#password').value
+                    alias: _page.querySelector('#username').value,
+                    password: _page.querySelector('#password').value
                 };
                 console.log(data);
                 ons.createDialog('dialogs/login_progress_dialog.html')
@@ -37,67 +74,111 @@
                         app.login(data, function(result) {
                             dialog.hide();
                             if (result) {
-                                document.querySelector('#navigator').pushPage('pages/lobby.html');
+                                document.querySelector('#navigator')
+                                        .pushPage('pages/lobby.html');
                             } else {
                                 ons.notification.alert('Login failed!');
                             }
                         });
                     });
-                
             };
-        },
-        
-        'game-page': function(page) {
-            page.querySelector('ons-toolbar .center').innerHTML = 'Game';
-            var rendererParent = document.getElementById("renderer_parent");
+            
+            return {
+                init: function(page) {
+                    _page = page;
+                    page.querySelector('ons-toolbar .center').innerHTML = 'Login';
+                    page.querySelector('#cancel-btn').onclick = function() {
+                        document.querySelector('#navigator').popPage();
+                    };
+                    page.querySelector('#loginbtn').onclick = _handleLogin;
+                },
                 
-            var game = new Chess();
-            var board = new BoardView(game, rendererParent, startGame);
-            var currentColor = 'w';
-            
-            function startGame() {
-                board.selectMoveForColor(currentColor, performMove);
+                show: function(page) {
+                    
+                },
+                
+                hide: function(page) {
+                    
+                },
+                
+                destroy: function(page) {
+                    
+                }
             }
             
-            function performMove(move) {
-                game.move(move);
-                board.animateMove(move, onFinishedAnimating);
-            }
-            
-            function onFinishedAnimating() {
-                nextColor();
-                board.selectMoveForColor(currentColor, performMove);
-            }
-            
-            function nextColor() {
-                currentColor = (currentColor === 'w') ? 'b' : 'w';
-            }
-            
-            var camControls = new CameraControls(board, rendererParent);
-        },
+        })(),
         
-        'lobby-page': function(page) {
-            var list = page.querySelector('#lobby-player-list');
-            var players = [];
-            var waitingDialog;
+        'game-page': (function() {
             
-            var acceptInvite = function(userId) {
+            return {
+                init: function(page) {
+                    page.querySelector('ons-toolbar .center').innerHTML = 'Game';
+                    var rendererParent = document.getElementById("renderer_parent");
+                        
+                    var game = new Chess();
+                    var board = new BoardView(game, rendererParent, startGame);
+                    var currentColor = 'w';
+                    
+                    function startGame() {
+                        board.selectMoveForColor(currentColor, performMove);
+                    }
+                    
+                    function performMove(move) {
+                        game.move(move);
+                        board.animateMove(move, onFinishedAnimating);
+                    }
+                    
+                    function onFinishedAnimating() {
+                        nextColor();
+                        board.selectMoveForColor(currentColor, performMove);
+                    }
+                    
+                    function nextColor() {
+                        currentColor = (currentColor === 'w') ? 'b' : 'w';
+                    }
+                    
+                    var camControls = new CameraControls(board, rendererParent);
+                },
+                
+                show: function(page) {
+                    
+                },
+                
+                hide: function(page) {
+                    
+                },
+                
+                destroy: function(page) {
+                    
+                }
+            }
+            
+        })(),
+        
+        'lobby-page': (function() {
+            
+            var _dialog;
+            var _players = [];
+            var _list;
+            
+            var _acceptInvite = function(userId) {
                 ons.createDialog('dialogs/login_progress_dialog.html')
                     .then(function(dialog) {
-                        waitingDialog = dialog;
-                        dialog.querySelector('.dialog-caption').innerHTML = 'Waiting for opponent...';
+                        _dialog = dialog;
+                        dialog.querySelector('.dialog-caption')
+                                .innerHTML = 'Waiting for opponent...';
                         dialog.show();
                         app.acceptInvite(userId);
                     });
             };
             
-            var onGameInvite = function(msg) {
+            var _handleGameInvite = function(msg) {
                 ons.notification.confirm({
                     message: 'Invitation from ' + msg.userId,
                     buttonLabels: ['Decline', 'Accept'],
                     callback: function(selected) {
                         if (selected == 1) {
-                            acceptInvite(msg.userId);
+                            _acceptInvite(msg.userId);
                         } else {
                             app.declineInvite(msg.userId);
                         }
@@ -105,31 +186,61 @@
                 });
             };
             
-            var onGameLaunch = function(msg) {
+            var _handleGameLaunch = function(msg) {
                 console.log('game_launch received');
-                waitingDialog.hide();
-                waitingDialog = null;
-                document.querySelector('#navigator').pushPage('pages/game.html');
+                _dialog.hide();
+                _dialog = null;
+                document.querySelector('#navigator')
+                        .pushPage('pages/game.html');
             };
             
-            var onGameResponse = function(msg) {
+            var _handleGameResponse = function(msg) {
                 console.log('Response:');
                 console.log(msg);
                 if (msg.accepted) {
                     app.launchGame();
-                    document.querySelector('#navigator').pushPage('pages/game.html');
+                    document.querySelector('#navigator')
+                            .pushPage('pages/game.html');
                 } else {
                     ons.notification.alert(msg.id + 'declined your invitation!');
                 }
             };
             
-            list.delegate = {
+            var _handleJoinLobby = function(msg) {
+                _players.push(msg.id);
+                _list.refresh();
+            };
+            
+            var _handleLeaveLobby = function(msg) {
+                var i = _players.indexOf(msg.id);
+                if (i != -1)
+                    _players.splice(i, 1);
+                _list.refresh();
+            };
+            
+            var _reloadList = function() {
+                app.queryPlayersInLobby(function(users) {
+                    if (users != null) {
+                        console.log(users.length);
+                        _players = users;
+                        _list.refresh();
+                    } else {
+                        console.log('no users');
+                    }
+                    
+                    app.joinLobby();
+                });
+            };
+            
+            var _listDelegate = {
                 createItemContent: function(i) {
-                    var id = players[i];
+                    var id = _players[i];
                     var item = ons._util.createElement(
                         '<ons-list-item tappable="">\
                             <div class="left">\
-                                <ons-icon icon="md-face" class="list-item__icon"></ons-icon>\
+                                <ons-icon icon="md-face"\
+                                    class="list-item__icon">\
+                                </ons-icon>\
                             </div>\
                             <div class="center">'
                                 + id +
@@ -145,41 +256,79 @@
                 },
                 
                 countItems: function() {
-                    return players.length;
+                    return _players.length;
                 }
             };
             
-            app.queryPlayersInLobby(function(users) {
-                if (users != null) {
-                    console.log(users.length);
-                    players = users;
-                    list.refresh();
-                } else {
-                    console.log('no users');
-                }
+            return {
+                init: function(page) {
+                    _list = page.querySelector('#lobby-player-list');
+                    _list.delegate = _listDelegate;
+                },
                 
-                app.joinLobby();
-            });
+                show: function(page) {
+                    _reloadList();
+                    app.on('game_invite', _handleGameInvite);
+                    app.on('game_launch', _handleGameLaunch);
+                    app.on('game_response', _handleGameResponse);
+                    app.on('join_lobby', _handleJoinLobby);
+                    app.on('leave_lobby', _handleLeaveLobby);
+                },
+                
+                hide: function(page) {
+                    app.removeListener('game_invite', _handleGameInvite);
+                    app.removeListener('game_launch', _handleGameLaunch);
+                    app.removeListener('game_response', _handleGameResponse);
+                    app.removeListener('join_lobby', _handleJoinLobby);
+                    app.removeListener('leave_lobby', _handleLeaveLobby);
+                    app.leaveLobby();
+                },
+                
+                destroy: function(page) {
+                }
+            }
             
-            app.on('game_invite', onGameInvite);
-            app.on('game_launch', onGameLaunch);
-            app.on('game_response', onGameResponse);
-            app.on('join_lobby', function(msg) {
-                players.push(msg.id);
-                list.refresh();
-            });
-        }
-        
+        })(),
     };
 
     ons.ready(function() {
         app = new App();
     });
-        
+    
+    /*
     document.addEventListener('init', function(event) {
         var page = event.target;
         if (pageInitializers.hasOwnProperty(page.id)) {
             pageInitializers[page.id](page);
+        }
+    });
+    */
+    
+    document.addEventListener('init', function(event) {
+        var page = event.target;
+        if (pages.hasOwnProperty(page.id) && pages[page.id].init != null) {
+            pages[page.id].init(page);
+        }
+    });
+    
+    document.addEventListener('show', function(event) {
+        var page = event.target;
+        if (pages.hasOwnProperty(page.id) && pages[page.id].show != null) {
+            pages[page.id].show(page);
+        }
+    });
+    
+    document.addEventListener('hide', function(event) {
+        var page = event.target;
+        if (pages.hasOwnProperty(page.id) && pages[page.id].hide != null) {
+            pages[page.id].hide(page);
+        }
+    });
+    
+    document.addEventListener('destroy', function(event) {
+        var page = event.target;
+        if (pages.hasOwnProperty(page.id) && pages[page.id].destroy != null) {
+            pages[page.id].destroy(page);
         }
     });
 })();
